f8fdc2aad0a52f2371da30c9533a830abc2e3d5d
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2026-07-06 15:10:27 -0300

    sssssssssssssssss

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function verificaIdade() {
     let idade = prompt("Qual é a sua idade?");
     
     if(idade >= 18) {
-        console.log("já pode dirigir");
+        alert("já pode dirigir");
     } else {
-        console.log("não pode dirigir");
+        alert("não pode dirigir");
     }
 }
